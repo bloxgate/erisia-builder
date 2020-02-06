ce24ba17f8e92893bc3f5ea55beb17684e39792b
Svein Ove Aas <svein@google.com>
2020-02-06 20:24:39 +0000

    Remove old (nonexistent & unused) resources

--- default.nix
+++ default.nix
@@ -5,33 +5,21 @@ with import ./lib/lib.nix;
 with import ./lib/sprocket;
 
 let resources_12 = runLocally "resources-1.12" {
-      sphax = fetchurl {
-        url = https://madoka.brage.info/baughn/Sphax64x_FTB_Revelation.zip;
-        sha256 = "03jdl0y2z00gnvc18gw69jcx08qigf6fbj09ii21gikh1fg4imfi";
-      };
     } ''
       mkdir -p $out/resourcepacks $out/shaderpacks
       #ln -s $seus $out/shaderpacks/SEUS-v11.0.zip
       #ln -s $faithful $out/resourcepacks/F32-1.10.2.zip
-      ln -s $sphax $out/resourcepacks/Sphax.zip
+      #ln -s $sphax $out/resourcepacks/Sphax.zip
     '';
    resources_10 = runLocally "resources-1.10" {
-     ozocraft = fetchurl {
-       url = https://madoka.brage.info/baughn/OzoCraft-1.10a.zip;
-       sha256 = "07xkg39idnp99gn4v7c2nc2sc9a948h16nsan4f68h3ncyshxbgs";
-     };
    } ''
      mkdir -p $out/resourcepacks
-     ln -s $ozocraft $out/resourcepacks/OzoCraft.zip
+     #ln -s $ozocraft $out/resourcepacks/OzoCraft.zip
    '';
    resources_7 = runLocally "resources-1.7" {
-     erisia = fetchurl {
-       url = https://madoka.brage.info/baughn/erisia-pack.zip;
-       sha256 = "17hy9hf948xkj3wgkr1q2q8qr2cqgkvy7d0zii89vrzai9xw0aif";
-     };
    } ''
      mkdir -p $out/resourcepacks
-     ln -s $erisia $out/resourcepacks/erisia-pack.zip
+     #ln -s $erisia $out/resourcepacks/erisia-pack.zip
    '';
 in
 
