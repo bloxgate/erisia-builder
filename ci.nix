e101142d90c896a0660c4800ae9a2e16b96cb700
Svein Ove Aas <svein@google.com>
2020-02-08 20:35:16 +0000

    Add ci.nix for Hercules-CI

---
+++ ci.nix
@@ -0,0 +1,11 @@
+with import ./nixpkgs {};
+
+let builder = import ./default.nix;
+in {
+  # All server derivations. These are the ones that get run.
+  servers = pkgs.recurseIntoAttrs (lib.mapAttrs (name: server: server.server) builder.packs);
+
+  # The ServerPack recursively includes everything used client-side.
+  # Let's build the website, too.
+  inherit (builder) ServerPack web;
+}
