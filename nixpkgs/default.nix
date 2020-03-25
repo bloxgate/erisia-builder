ea7a0823ebe107670d9eb616c2b8d57a10db6d61
Erisia administrator <minecraft@madoka.brage.info>
2020-03-25 10:37:11 +0000

    Bump nixpkgs to current 20.03

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,8 +1,9 @@
 import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
   name = "nixos-minecraft";
-  # Commit hash for nixos-unstable as of 2020-02-05 (or so)
-  url = https://github.com/nixos/nixpkgs/archive/a21c2fa3ea2b88e698db6fc151d9c7259ae14d96.tar.gz;
+  # Commit hash for nixos-20.03 as of 2020-03-19
+  # Obtained from https://status.nixos.org/
+  url = https://github.com/nixos/nixpkgs/archive/b2935fbeceaea0b64df4401545d7c8ea29102120.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1z3kxlbz6bqx1dlagcazg04vhk67r8byihzf959c3m0laf2a1w7y";
+  sha256 = "1z1631v7q2c8mavy7xnvfx0wz34zd49jqmjg66nk0qgsi605m3qp";
 })
