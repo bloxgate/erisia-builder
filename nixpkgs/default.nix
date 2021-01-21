e3e33a33df361ad3efa8e75bed946eee81b578c3
Erisia administrator <minecraft@madoka.brage.info>
2021-01-21 13:35:17 +0000

    Bump nixpkgs

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,9 +1,9 @@
 import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
   name = "nixos-minecraft";
-  # Commit hash for nixos-20.03 as of 2020-03-19
+  # Commit hash for nixos-20.09 as of 2021-01-21
   # Obtained from https://status.nixos.org/
-  url = https://github.com/nixos/nixpkgs/archive/b2935fbeceaea0b64df4401545d7c8ea29102120.tar.gz;
+  url = https://github.com/nixos/nixpkgs/archive/646f528cce6db1f89f6e635a7589bf1be6169ffb.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1z1631v7q2c8mavy7xnvfx0wz34zd49jqmjg66nk0qgsi605m3qp";
+  sha256 = "1nkpk6dzwblb76fgd8grs4d4fkwlzb4kxp7kvhdp4jzvkks2g1v5";
 })
--- tools/control/default.nix
+++ tools/control/default.nix
@@ -8,5 +8,5 @@ rustPlatform.buildRustPackage rec {
     (path: type: type != "directory" || baseNameOf path == "src")
     ./.;
 
-  cargoSha256 = "1kyhsgbv9bxbw3k6sxwzkxb1ix85ica43vwwkzyd5plk75jpqx6g";
+  cargoSha256 = "1ysj7kvdhgc811sw8vcn3z707lj31jj25kwwmcbp6k5kk4w3h6p9";
 }
