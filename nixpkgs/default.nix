bd17886d5b57978854865ee4f87fe7b623ebc79e
Erisia administrator <minecraft@madoka.brage.info>
2021-05-17 01:59:50 +0100

    Bump nixpkgs version

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -3,7 +3,7 @@ import (builtins.fetchTarball {
   name = "nixos-minecraft";
   # Commit hash for nixos-20.09 as of 2021-01-21
   # Obtained from https://status.nixos.org/
-  url = https://github.com/nixos/nixpkgs/archive/646f528cce6db1f89f6e635a7589bf1be6169ffb.tar.gz;
+  url = https://github.com/NixOS/nixpkgs/archive/da7f4c4842520167f65c20ad75ecdbd14e27ae91.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1nkpk6dzwblb76fgd8grs4d4fkwlzb4kxp7kvhdp4jzvkks2g1v5";
+  sha256 = "0vdq6lkc1sqj85x8r8idpck3igjns8ix57fqf1r5pm4k0qhy7p2m";
 })
