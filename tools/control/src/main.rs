977b95ad2ab26f32c45c037c8755727750086a31
Erisia administrator <minecraft@madoka.brage.info>
2021-07-05 01:56:52 +0100

    control: Restart unconditionally if playerdata is inaccessible

--- tools/control/src/main.rs
+++ tools/control/src/main.rs
@@ -47,19 +47,25 @@ impl Server {
     }
 
     async fn get(&self, metric: &str) -> Result<f64> {
-        let body: String = reqwest::get(&format!(
+        let request = reqwest::get(&format!(
             "http://localhost:{}/metrics",
             self.prometheus_port
         ))
-        .await?
-        .text()
-        .await?;
-
-        for line in body.split_terminator('\n') {
-            let segment: Vec<&str> = line.split(' ').collect();
-            if segment[0].starts_with(metric) {
-                return Ok(segment[1].parse()?);
+        .await;
+        if let Ok(request) = request {
+            let body = request.text().await;
+            if let Ok(body) = body {
+                for line in body.split_terminator('\n') {
+                    let segment: Vec<&str> = line.split(' ').collect();
+                    if segment[0].starts_with(metric) {
+                        return Ok(segment[1].parse()?);
+                    }
+                }
+            } else {
+                println!("Could not get playerdata: {:?}", body);
             }
+        } else {
+            println!("Could not get playerdata: {:?}", request);
         }
         Ok(0.0)
     }
